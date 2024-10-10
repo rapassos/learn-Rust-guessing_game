5647d3d7e1f6a8c16fdd0bead8e19712a5729037
Rafael Passos <rapassos@gmail.com>
2024-10-10 13:05:22 -0300

    Ajuste de mensagens e comparação de valores

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Adivinhe o número!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("O número secreto é: {secret_number}");
+
+
+    println!("Por favor, insira seu palpite.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Falha ao ler a linha");
+
+    let guess: u32 = guess.trim().parse().expect("Por favor, digite um número!");
+
+    println!("Seu palpite é: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("{} é menor que o número secreto",guess.to_string()),
+        Ordering::Greater => println!("{} Maior que o numero secreto",guess.to_string()),
+        Ordering::Equal => println!("Voce acertou, parabéns!"),
+    }
+
 }
