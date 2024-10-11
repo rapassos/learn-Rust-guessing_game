5cbd0279c669a4cbf82935a2e58305fb37c84bc0
Rafael Passos <rapassos@gmail.com>
2024-10-11 02:40:15 -0300

    Inclusão do looping para entradas de palpites com a saída ao acertar o número secreto

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Adivinhe o número!");
@@ -9,23 +9,26 @@ fn main() {
 
     println!("O número secreto é: {secret_number}");
 
+    loop {
+        println!("Por favor, insira seu palpite.");
 
-    println!("Por favor, insira seu palpite.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Falha ao ler a linha");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Falha ao ler a linha");
+        let guess: u32 = guess.trim().parse().expect("Por favor, digite um número!");
 
-    let guess: u32 = guess.trim().parse().expect("Por favor, digite um número!");
+        println!("Seu palpite é: {}", guess);
 
-    println!("Seu palpite é: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("{} é menor que o número secreto",guess.to_string()),
-        Ordering::Greater => println!("{} Maior que o numero secreto",guess.to_string()),
-        Ordering::Equal => println!("Voce acertou, parabéns!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{} é menor que o número secreto", guess.to_string()),
+            Ordering::Greater => println!("{} Maior que o numero secreto", guess.to_string()),
+            Ordering::Equal => {
+                println!("Voce acertou, parabéns!");
+                break;
+            }
+        }
     }
-
 }
